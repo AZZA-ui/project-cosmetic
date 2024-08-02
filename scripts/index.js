7fc388212207df0b3fdd9019e1ef809fc1f02959
AZZA-ui <mamirovaa20@gmail.com>
2024-08-02 10:17:28 +0600

    add new function map in js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,49 @@
-let link = document.querySelector(".menu-item > a");
-for(let i = 0; i < links.length; i++ ) {
-    links [i].onclick = function  (event) {
-        event.preventDefault();
-        document
-        .querySelector(links[i].getAttribute("href"))
-        .scrollIntoView({ behavior: "smooth"})
-    };
-}  
+let links = document.querySelector(".menu-item > a");
+for (let i = 0; i < links.length; i++) {
+  links[i].onclick = function (event) {
+    event.preventDefault();
+    document
+      .querySelector(links[i].getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  };
+}
+const products = [
+  {
+    id: 1,
+    name: "Cosmetic products",
+    descrtion:
+      "Lorem Ipsum is simply dum    my text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+    image: "./assets/image/1.png",
+    price: "120",
+  },
+  {
+    id: 2,
+    name: "Cosmetic products",
+    descrtion:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+    image: "./assets/image/2.png",
+    price: "120",
+  },
+];
+const productsBox = document.querySelector(".products-box");
+const productMap = products.map((el) => {
+  return `
+       <div class="card">
+        <img src="${el.image}" alt="pump" loading="lazy"/>
+          <div class="card-content">
+             <h2>${el.name  }</h2>
+             <p>${el.descrtion}</p>
+          <div class="card-actions">
+             <div class="card-action">
+                <button>Buy Now</button>
+                <button>Add To Cart</button>
+            </div>
+                  <span>${el.price}$</span>
+                </div>
+              </div>
+            </div>
+    `;
+});
+
+productsBox.innerHTML = productMap.join("");
+console.log(productMap);
